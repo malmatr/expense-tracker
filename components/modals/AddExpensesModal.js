e97280af2f98f151971ba9419db954fe295da17d
mallory matravers <matraversmallory@gmail.com>
2026-02-17 19:39:14 -0700

    sixth commit pt 2

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -1,4 +1,6 @@
-import { useState, useContext } from "react";
+"use client";
+
+import { useState, useContext, useRef } from "react";
 import { financeContext } from "@/lib/store/finance-context";
 
 import { v4 as uuidv4 } from "uuid";
@@ -9,10 +11,14 @@ import { color } from "chart.js/helpers";
 function AddExpensesModal({ show, onClose }) {
   const [expenseAmount, setExpenseAmount] = useState("");
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [showAddExpense, setShowAddExpense] = useState(false);
 
-  const { expenses } = useContext(financeContext);
+  const { expenses, addExpenseItem, addCategory } = useContext(financeContext);
 
-  const addExpenseItemHandler = () => {
+  const titleRef = useRef();
+  const colorRef = useRef();
+
+  const addExpenseItemHandler = async () => {
     const expense = expenses.find((e) => {
       return e.id === selectedCategory;
     });
@@ -30,10 +36,29 @@ function AddExpensesModal({ show, onClose }) {
         },
       ],
     };
-    console.log(newExpense);
-    setExpenseAmount("");
-    setSelectedCategory(null);
-    onClose();
+
+    try {
+      await addExpenseItem(selectedCategory, newExpense);
+
+      console.log(newExpense);
+      setExpenseAmount("");
+      setSelectedCategory(null);
+      onClose();
+    } catch (error) {
+      console.log(error.mesage);
+    }
+  };
+
+  const addCategoryHandler = async () => {
+    const title = titleRef.current.value;
+    const color = colorRef.current.value;
+
+    try {
+      await addCategory({ title, color, total: 0 });
+      setShowAddExpense(false);
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
@@ -55,7 +80,35 @@ function AddExpensesModal({ show, onClose }) {
       {/* { Expense Categories } */}
       {expenseAmount > 0 && (
         <div className="flex flex-col gap-4 mt-6">
-          <h3 className="text-2xl capitalize">Select expense category</h3>
+          <div className="flex items-center justify-between">
+            <h3 className="text-2xl capitalize">Select expense category</h3>
+            <button
+              onClick={() => {
+                setShowAddExpense(true);
+              }}
+              className="text-lime-400"
+            >
+              + New category
+            </button>
+          </div>
+
+          {showAddExpense && (
+            <div className="flex items-center justify-between">
+              <input type="text" placeholder="Enter Title" ref={titleRef} />
+              <label>Pick Color</label>
+              <input type="color" className="w-24 h-10" ref={colorRef} />
+              <button className="btn btn-primary-outline">Create</button>
+              <button
+                onClick={() => {
+                  setShowAddExpense(false);
+                }}
+                className="btn btn-danger"
+              >
+                Cancel
+              </button>
+            </div>
+          )}
+
           {expenses.map((expense) => {
             return (
               <button
---
+++ components/modals/ViewExpenseModal.js
@@ -0,0 +1,73 @@
+import { useContext } from "react";
+import { financeContext } from "@/lib/store/finance-context";
+
+import Modal from "@/components/Modal";
+import { currencyFormatter } from "@/lib/utils";
+
+import { FaRegTrashAlt } from "react-icons/fa";
+
+function ViewExpenseModal({ show, onClose, expense }) {
+  const { deleteExpenseItem, deleteExpenseCategory } =
+    useContext(financeContext);
+
+  const deleteExpenseHandler = async () => {
+    try {
+      await deleteExpenseCategory(expense.id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const deleteExpenseItemHandler = async (item) => {
+    try {
+      const updatedItems = expense.items.filter((i) => i.id !== item.id);
+
+      const updatedExpense = {
+        items: [...updatedItems],
+        total: expense.total - item.amount,
+      };
+
+      await deleteExpenseItem(updatedExpense, expense.id);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <Modal show={show} onClose={onClose}>
+      <div className="flex items-center justify-between">
+        <h2 className="text-4xl">{expense.title}</h2>
+        <button onClick={deleteExpenseHandler} className="btn btn-danger">
+          Delete
+        </button>
+      </div>
+
+      <div>
+        <h3 className="my-4 text-2xl">Expense History</h3>
+        {expense.items.map((items) => {
+          return (
+            <div key={items.id} className="flex items-center justify-between">
+              <small>
+                {items.createdAt.toMillis
+                  ? new Date(items.createdAt.toMillis()).toISOString()
+                  : items.createdAt.toISOString()}
+              </small>
+              <p className="flex items-center gap-2">
+                {currencyFormatter(items.amount)}
+              </p>
+              <button
+                onClick={() => {
+                  deleteExpenseItemHandler(item);
+                }}
+              >
+                <FaRegTrashAlt />
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </Modal>
+  );
+}
+
+export default ViewExpenseModal;
